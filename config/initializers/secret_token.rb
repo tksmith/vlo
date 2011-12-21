77886e326562d51a0fb210adf4bc7e6a410a0037
Kyle Smith <tksmith@crimson.ua.edu>
2011-12-21 14:01:08 -0600

    adding config folder contents

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vlo::Application.config.secret_token = 'c55af05a33a127f82450b5f28fb7134ef0d4a9fe9da4a058ca56b553773c9558c31fbe4a90f13565102774976b485013096419bc77ee3c18ae183b4b96fd6723'
