d99566916ee52eee9c8afa128bd7a5b066580606
Kyle Smith <tksmith@crimson.ua.edu>
2012-01-20 18:12:10 -0600

    fleshed out the profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :remember_me, :title, :suffix, :middle_name
+  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :remember_me, :title, :suffix, :middle_name, :nickname, :street_address, :city, :zip, :state, :phone, :website, :twitter, :best_way_to_contact, :represented_before, :circumstance, :consulted_attorney, :why_not_hire_attorney, :legal_services_needed, :parties_involved, :parties
   validates_presence_of :first_name
   validates_presence_of :last_name
 end
